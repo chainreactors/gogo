547dd99ba957ce09316a4402181187c3696dafb7
09ic m <M09ic>
2020-10-28 22:58:43 +0800

    fix a bug

--- gox.bat
+++ gox.bat
@@ -1,4 +1,5 @@
+
 gox.exe -osarch="linux/amd64 linux/386 windows/amd64 windows/386 darwin/amd64" -output=".\bin\getitle_{{.OS}}_{{.Arch}}%1" .\src\main\
 upx ./bin/getitle_windows_386%1.exe
 upx ./bin/getitle_windows_amd64%1.exe
-tar cvf getitle%1.zip bin/*
+tar cvf bin/getitle%1.zip bin/*
