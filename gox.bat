0355eab4de806a80696d7dfbaf0096d40a1f9b6b
M09ic <M09ic>
2020-10-29 00:41:27 +0800

    repush

--- gox.bat
+++ gox.bat
@@ -2,4 +2,4 @@
 gox.exe -osarch="linux/amd64 linux/386 windows/amd64 windows/386 darwin/amd64" -output=".\bin\getitle_{{.OS}}_{{.Arch}}%1" .\src\main\
 upx ./bin/getitle_windows_386%1.exe
 upx ./bin/getitle_windows_amd64%1.exe
-tar cvf bin/getitle%1.zip bin/*
+tar cvf release/getitle%1.zip bin/*
