ff7aa6bfe2a0ae33cae3b3d9ff56079f039cf58b
M09ic <M09ic>
2020-11-27 17:14:57 +0800

    修复nbtscan与oxid的扫描结果显示bug

--- FingerprintUpdate.py
+++ FingerprintUpdate.py
@@ -23,5 +23,5 @@ if __name__ == "__main__":
 	}
 	'''
 	f.write(base%json.dumps(j))
-	print("finger update success")
+	print("fingerprint update success")
 
--- gox.bat
+++ gox.bat
@@ -1,4 +1,4 @@
-python FingerUpdate.py
+python FingerprintUpdate.py
 gox.exe -osarch="linux/amd64 linux/386 windows/amd64 windows/386 darwin/amd64" -ldflags="-s -w" -output=".\bin\getitle_{{.OS}}_{{.Arch}}_v%1" .\src\main\
 upx -9 -k -o ./bin/getitle_windows_386_v%1_upx.exe ./bin/getitle_windows_386_v%1.exe
 upx -9 -k -o ./bin/getitle_windows_amd64_v%1_upx.exe ./bin/getitle_windows_amd64_v%1.exe
