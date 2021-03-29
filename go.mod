bcf8c5c0bae4a2b198735fbe797065db2db827b4
M09ic <m09ician@gmail.com>
2021-03-29 13:49:51 +0800

    优化指纹,新增kscan中的mmh3favicon指纹

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module getitle
 
 go 1.10
 
-require github.com/panjf2000/ants/v2 v2.4.2
+require (
+	github.com/panjf2000/ants/v2 v2.4.2
+	github.com/twmb/murmur3 v1.1.5
+)
--- src/Scan/FaviconScan.go
+++ src/Scan/FaviconScan.go
@@ -1,39 +1,81 @@
 package Scan
 
 import (
+	"bytes"
 	"crypto/md5"
+	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"getitle/src/Utils"
+	"github.com/twmb/murmur3"
 	"os"
 )
 
 // -v
 // 信息收集插件,通过匹配http服务的favicon md5值判断CMS
 func FaviconScan(result *Utils.Result) {
-	var fingers map[string]interface{}
-
-	err := json.Unmarshal([]byte(Utils.LoadFingers("md5")), &fingers)
-	if err != nil {
-		println("[-] md5 fingers load FAIL!")
-		os.Exit(0)
-	}
-
+	var err error
 	conn := Utils.HttpConn(2)
-	resp, err := conn.Get(Utils.GetURL(result) + "/favicon.ico")
+	url := Utils.GetURL(result)
+	resp, err := conn.Get(url + "/favicon.ico")
 	if err != nil {
 		return
 	}
 	if resp.StatusCode != 200 {
 		return
 	}
-	content := string(Utils.GetBody(resp))
-	m := md5.Sum([]byte(content))
-	ms := hex.EncodeToString(m[:])
+	content := Utils.GetBody(resp)
 
-	if fingers[ms] != nil {
-		result.Framework = fingers[ms].(string)
+	// MD5 hash匹配
+	var md5fingers map[string]string
+	err = json.Unmarshal([]byte(Utils.LoadFingers("md5")), &md5fingers)
+	if err != nil {
+		println("[-] md5 fingers load FAIL!")
+		os.Exit(0)
+	}
+	md5h := md5Hash(content)
+	if md5fingers[md5h] != "" {
+		result.Framework = md5fingers[md5h]
+		return
+	}
+
+	// mmh3 hash匹配,指纹来自kscan
+	var mmh3fingers map[string]string
+	err = json.Unmarshal([]byte(Utils.LoadFingers("mmh3")), &mmh3fingers)
+	if err != nil {
+		println("[-] mmh3 fingers load FAIL!")
+		os.Exit(0)
+	}
+	mmh3h := mmh3Hash32(content)
+	if mmh3fingers[mmh3h] != "" {
+		result.Framework = mmh3fingers[mmh3h]
 		return
 	}
 	return
+}
+
+func md5Hash(raw []byte) string {
+	m := md5.Sum(raw)
+	return hex.EncodeToString(m[:])
+}
+
+func mmh3Hash32(raw []byte) string {
+	var h32 = murmur3.New32()
+	_, _ = h32.Write(standBase64(raw))
+	return fmt.Sprintf("%d", h32.Sum32())
+}
+
+func standBase64(braw []byte) []byte {
+	bckd := base64.StdEncoding.EncodeToString(braw)
+	var buffer bytes.Buffer
+	for i := 0; i < len(bckd); i++ {
+		ch := bckd[i]
+		buffer.WriteByte(ch)
+		if (i+1)%76 == 0 {
+			buffer.WriteByte('\n')
+		}
+	}
+	buffer.WriteByte('\n')
+	return buffer.Bytes()
 }
