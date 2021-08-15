b37a41b19be83d921ae97c56d93638c722b55b65
M09ic <m09ician@gmail.com>
2021-08-15 22:44:34 +0800

    优化gox.bat 编译脚本

--- gox.bat
+++ gox.bat
@@ -1,25 +1,25 @@
 python FingerprintUpdate.py
+set name=getitle
+gox.exe -osarch="linux/amd64 linux/arm64 linux/386 windows/amd64 linux/mips64 windows/386 darwin/amd64" -ldflags="-s -w" -gcflags="-trimpath=$GOPATH" -asmflags="-trimpath=$GOPATH" -output=".\bin\%name%_{{.OS}}_{{.Arch}}_v%1" .\src\main\
 
-gox.exe -osarch="linux/amd64 linux/arm64 linux/386 windows/amd64 linux/mips64 windows/386 darwin/amd64" -ldflags="-s -w" -gcflags=-trimpath=$GOPATH;$GOROOT -asmflags=-trimpath=$GOPATH -output=".\bin\getitle_{{.OS}}_{{.Arch}}_v%1" .\src\main\
-
-go-strip -f ./bin/getitle_windows_386_v%1.exe -a -output ./bin/getitle_windows_386_v%1.exe > nul
-go-strip -f ./bin/getitle_windows_amd64_v%1.exe -a -output ./bin/getitle_windows_amd64_v%1.exe > nul
-go-strip -f ./bin/getitle_linux_386_v%1 -a -output ./bin/getitle_linux_386_v%1 > nul
-go-strip -f ./bin/getitle_linux_arm64_v%1 -a -output ./bin/getitle_linux_arm64_v%1 > nul
-go-strip -f ./bin/getitle_linux_amd64_v%1 -a -output ./bin/getitle_linux_amd64_v%1 > nul
-go-strip -f ./bin/getitle_linux_mips64_v%1 -a -output ./bin/getitle_linux_mips64_v%1 > nul
-go-strip -f ./bin/getitle_darwin_amd64_v%1 -a -output ./bin/getitle_darwin_amd64_v%1 > nul
+go-strip -f ./bin/%name%_windows_386_v%1.exe -a -output ./bin/%name%_windows_386_v%1.exe > nul
+go-strip -f ./bin/%name%_windows_amd64_v%1.exe -a -output ./bin/%name%_windows_amd64_v%1.exe > nul
+go-strip -f ./bin/%name%_linux_386_v%1 -a -output ./bin/%name%_linux_386_v%1 > nul
+go-strip -f ./bin/%name%_linux_arm64_v%1 -a -output ./bin/%name%_linux_arm64_v%1 > nul
+go-strip -f ./bin/%name%_linux_amd64_v%1 -a -output ./bin/%name%_linux_amd64_v%1 > nul
+go-strip -f ./bin/%name%_linux_mips64_v%1 -a -output ./bin/%name%_linux_mips64_v%1 > nul
+go-strip -f ./bin/%name%_darwin_amd64_v%1 -a -output ./bin/%name%_darwin_amd64_v%1 > nul
 
 
 
-upxs -1 -k -o ./bin/getitle_windows_386_v%1_upx.exe ./bin/getitle_windows_386_v%1.exe
-upxs -1 -k -o ./bin/getitle_windows_amd64_v%1_upx.exe ./bin/getitle_windows_amd64_v%1.exe
-upxs -1 -k -o ./bin/getitle_linux_386_v%1_upx ./bin/getitle_linux_386_v%1
-upxs -1 -k -o ./bin/getitle_linux_amd64_v%1_upx ./bin/getitle_linux_amd64_v%1
-upxs -1 -k -o ./bin/getitle_linux_arm64_v%1_upx ./bin/getitle_linux_arm64_v%1
-upxs -1 -k -o ./bin/getitle_darwin_amd64_v%1_upx ./bin/getitle_darwin_amd64_v%1
+upxs -1 -k -o ./bin/%name%_windows_386_v%1_upx.exe ./bin/%name%_windows_386_v%1.exe
+upxs -1 -k -o ./bin/%name%_windows_amd64_v%1_upx.exe ./bin/%name%_windows_amd64_v%1.exe
+upxs -1 -k -o ./bin/%name%_linux_386_v%1_upx ./bin/%name%_linux_386_v%1
+upxs -1 -k -o ./bin/%name%_linux_amd64_v%1_upx ./bin/%name%_linux_amd64_v%1
+upxs -1 -k -o ./bin/%name%_linux_arm64_v%1_upx ./bin/%name%_linux_arm64_v%1
+upxs -1 -k -o ./bin/%name%_darwin_amd64_v%1_upx ./bin/%name%_darwin_amd64_v%1
 
-limelighter -I ./bin/getitle_windows_amd64_v%1.exe -O ./bin/getitle_windows_amd64_sangfor_v%1.exe -Domain www.sangfor.com
+limelighter -I ./bin/%name%_windows_amd64_v%1.exe -O ./bin/%name%_windows_amd64_sangfor_v%1.exe -Domain www.sangfor.com
 rm *.sangfor.*
 
-tar cvf release/getitlev%1.tar bin/* README.md gtfilter.py
+tar cvf release/%name%v%1.tar bin/* README.md gtfilter.py
