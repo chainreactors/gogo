f27ddfb250546807abe4057384fbec6d6579b81a
M09ic <m09ician@gmail.com>
2021-12-06 10:37:55 +0800

    ms17-010扫描,新增doublepulsar后门探测

--- src/scan/ms17010Scan.go
+++ src/scan/ms17010Scan.go
@@ -95,7 +95,9 @@ func ms17010Scan(result *utils.Result) {
 	if reply[9] == 0x05 && reply[10] == 0x02 && reply[11] == 0x00 && reply[12] == 0xc0 {
 		result.Title = strings.Replace(os, "\x00", "", -1)
 		result.AddVuln(utils.Vuln{Name: "MS17-010"})
-		// detect present of DOUBLEPULSAR SMB implant
+		if reply[34] == 0x51 {
+			result.AddVuln(utils.Vuln{Name: "DOUBLEPULSAR"})
+		}
 	}
 	return
 }
--- src/structutils/utils.go
+++ src/structutils/utils.go
@@ -20,7 +20,7 @@ import (
 
 //获取当前时间
 func GetCurtime() string {
-	curtime := time.Now().Format("2006-01-02 15:04:05")
+	curtime := time.Now().Format("2006-01-02 15:04.05")
 	return curtime
 }
 
