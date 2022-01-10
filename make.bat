cca746058c5b4768c78052bfe648e89d8c620090
M09ic <m09ician@gmail.com>
2022-01-10 14:32:31 +0800

    linux版本将只发布upx版

--- make.bat
+++ make.bat
@@ -2,7 +2,7 @@ python updateconfig.py
 rm ./bin/*
 set name=getitle
 gox.exe -osarch="linux/amd64 linux/arm64 linux/386 windows/amd64 linux/mips64 windows/386 darwin/amd64" -ldflags="-s -w" -gcflags="-trimpath=$GOPATH" -asmflags="-trimpath=$GOPATH" -output=".\bin\%name%_{{.OS}}_{{.Arch}}" .
-@echo off
+echo off
 @REM  go strip 去除编译信息
 @REM go-strip -f ./bin/%name%_windows_386.exe -a -output ./bin/%name%_windows_386.exe > nul
 @REM go-strip -f ./bin/%name%_windows_amd64.exe -a -output ./bin/%name%_windows_amd64.exe > nul
@@ -11,14 +11,15 @@ go-strip -f ./bin/%name%_linux_arm64 -a -output ./bin/%name%_linux_arm64
 go-strip -f ./bin/%name%_linux_amd64 -a -output ./bin/%name%_linux_amd64
 go-strip -f ./bin/%name%_linux_mips64 -a -output ./bin/%name%_linux_mips64
 go-strip -f ./bin/%name%_darwin_amd64 -a -output ./bin/%name%_darwin_amd64
-@echo on
+echo on
 
 @REM upx 加壳
 upxs  -k -o ./bin/%name%_windows_386_upx.exe ./bin/%name%_windows_386.exe
 upxs  -k -o ./bin/%name%_windows_amd64_upx.exe ./bin/%name%_windows_amd64.exe
-upxs  -k -o ./bin/%name%_linux_386_upx ./bin/%name%_linux_386
-upxs  -k -o ./bin/%name%_linux_amd64_upx ./bin/%name%_linux_amd64
-upxs  -k -o ./bin/%name%_linux_arm64_upx ./bin/%name%_linux_arm64
+upxs  ./bin/%name%_darwin_amd64
+upxs  ./bin/%name%_linux_386
+upxs  ./bin/%name%_linux_amd64
+upxs  ./bin/%name%_linux_arm64
 
 @REM 伪造证书
 limelighter -I ./bin/%name%_windows_amd64.exe -O ./bin/%name%_windows_amd64s.exe -Domain www.sangfor.com
