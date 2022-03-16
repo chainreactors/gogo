a37d6d72e2993a4fe558b700839254bc10d1ba19
M09ic <m09ician@gmail.com>
2022-03-16 15:09:54 +0800

    优化hostscan

--- src/scan/hostScan.go
+++ src/scan/hostScan.go
@@ -16,6 +16,9 @@ func hostScan(result *pkg.Result) {
 		return
 	}
 
+	if resp.StatusCode != 200 {
+		return
+	}
 	content, body := pkg.GetHttpRaw(resp)
 	hash := pkg.Md5Hash([]byte(strings.TrimSpace(body)))[:4] // 因为头中经常有随机值, 因此hash通过body判断
 	if result.Hash != hash {
