acc8aeb4d4a93e7b24a3f34093f595e9ee3a8d20
M09ic <m09ician@gmail.com>
2022-03-16 13:19:38 +0800

    优化hostscan输出, 现在会以info在末尾输出

--- src/scan/hostScan.go
+++ src/scan/hostScan.go
@@ -3,6 +3,7 @@ package scan
 import (
 	"getitle/src/pkg"
 	"net/http"
+	"strings"
 )
 
 func hostScan(result *pkg.Result) {
@@ -15,8 +16,10 @@ func hostScan(result *pkg.Result) {
 		return
 	}
 
-	if resp.StatusCode == 200 {
-		content, body := pkg.GetHttpRaw(resp)
+	content, body := pkg.GetHttpRaw(resp)
+	hash := pkg.Md5Hash([]byte(strings.TrimSpace(body)))[:4] // 因为头中经常有随机值, 因此hash通过body判断
+	if result.Hash != hash {
 		pkg.CollectHttpInfo(result, resp, content, body)
+		result.AddVuln(&pkg.Vuln{Name: "host", Payload: map[string]interface{}{"hostname": result.HttpHost}, Severity: "info"})
 	}
 }
