cff11af13f6c56d410bc01cb3e32ad7c41d50efa
M09ic <m09ician@gmail.com>
2022-03-16 17:07:25 +0800

    格式化host扫描结果的title

--- src/scan/hostScan.go
+++ src/scan/hostScan.go
@@ -25,7 +25,7 @@ func hostScan(result *pkg.Result) {
 		content, body := pkg.GetHttpRaw(resp)
 		hash := pkg.Md5Hash([]byte(strings.TrimSpace(body)))[:4] // 因为头中经常有随机值, 因此hash通过body判断
 		if result.Hash != hash {
-			vuln.Detail[host] = pkg.GetTitle(content)
+			vuln.Detail[host] = pkg.AsciiEncode(pkg.GetTitle(content))
 		}
 	}
 	if len(vuln.Detail) > 0 {
