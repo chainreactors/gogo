7cdbd90aa782317d5f6377f52518c400c6673a43
M09ic <m09ician@gmail.com>
2022-04-14 15:59:37 +0800

    优化编译脚本, 最新版的getitle将自动上传至oss

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 python updateconfig.py
-rm ./bin/*
 set name=getitle
 set cert_domain=www.dbappsecurity.com.cn
+rm ./bin/*
 gox.exe -osarch="linux/amd64 linux/arm64 linux/386 windows/amd64 linux/mips64 windows/386 darwin/amd64" -ldflags="-s -w -X 'getitle/src/cmd.ver=v%1' -X 'getitle/src/cmd.k=%2'" -gcflags="-trimpath=$GOPATH" -asmflags="-trimpath=$GOPATH" -output=".\bin\%name%_{{.OS}}_{{.Arch}}" .
 echo off
 @REM  go strip 去除编译信息
@@ -28,11 +28,22 @@ limelighter -I ./bin/%name%_windows_amd64_upx.exe -O ./bin/%name%_windows_amd64_
 limelighter -I ./bin/%name%_windows_386_upx.exe -O ./bin/%name%_windows_386_upxs.exe -Domain %cert_domain%
 limelighter -I ./bin/%name%_windows_386.exe -O ./bin/%name%_windows_386s.exe -Domain %cert_domain%
 
-rm *.sangfor.*
 rm ./bin/%name%_windows_amd64.exe
 rm ./bin/%name%_windows_amd64_upx.exe
 rm ./bin/%name%_windows_386.exe
 rm ./bin/%name%_windows_386_upx.exe
+
+@REM 上传到阿里云
+
+ossutil cp ./bin/%name%_windows_386_upxs.exe oss://sangfor-release/fscan/windows_386_upxs.exe -f 
+ossutil cp ./bin/%name%_windows_386s.exe oss://sangfor-release/fscan/windows_386s.exe -f 
+ossutil cp ./bin/%name%_windows_amd64s.exe oss://sangfor-release/fscan/windows_amd64s.exe -f 
+ossutil cp ./bin/%name%_windows_amd64_upxs.exe  oss://sangfor-release/fscan/windows_amd64_upxs.exe -f 
+ossutil cp ./bin/%name%_linux_386 oss://sangfor-release/fscan/linux_386 -f 
+ossutil cp ./bin/%name%_linux_arm64  oss://sangfor-release/fscan/linux_arm64 -f 
+ossutil cp ./bin/%name%_linux_amd64 oss://sangfor-release/fscan/linux_amd64 -f 
+ossutil cp ./bin/%name%_linux_mips64 oss://sangfor-release/fscan/linux_mips64 -f 
+ossutil cp ./bin/%name%_darwin_amd64 oss://sangfor-release/fscan/darwin_amd64 -f 
 
 @REM 打包
 tar -zcvf release/%name%v%1.tar.gz bin/* doc/* README.md *.py UPDATELOG.md
