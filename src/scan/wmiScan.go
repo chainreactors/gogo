dd57468d1fd6ca136c8b95587665ceb2da22e8dc
M09ic <m09ician@gmail.com>
2022-04-15 14:03:26 +0800

    编码wmi payload

--- src/scan/wmiScan.go
+++ src/scan/wmiScan.go
@@ -7,7 +7,7 @@ import (
 	"github.com/M09ic/go-ntlmssp"
 )
 
-var payload = []byte{5, 0, 11, 3, 16, 0, 0, 0, 120, 0, 40, 0, 3, 0, 0, 0, 184, 16, 184, 16, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 160, 1, 0, 0, 0, 0, 0, 0, 192, 0, 0, 0, 0, 0, 0, 70, 0, 0, 0, 0, 4, 93, 136, 138, 235, 28, 201, 17, 159, 232, 8, 0, 43, 16, 72, 96, 2, 0, 0, 0, 10, 2, 0, 0, 0, 0, 0, 0, 78, 84, 76, 77, 83, 83, 80, 0, 1, 0, 0, 0, 7, 130, 8, 162, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 1, 177, 29, 0, 0, 0, 15}
+var data = pkg.Decode("YmXgZhZgYGCoYNBgYGZgYNghsAPEZWAEY0aGBSAGAwPDAQjlBiJYYju6XsucFJz/goNBW8AjgYmBgYGLCaLAL8THNzg4AKyfvYljEQMaYGPcKMvAwMAPAAAA//8=")
 
 func wmiScan(result *pkg.Result) {
 	result.Port = "135"
@@ -18,7 +18,7 @@ func wmiScan(result *pkg.Result) {
 	}
 	defer conn.Close()
 	result.Open = true
-	ret, err := pkg.SocketSend(conn, payload, 4096)
+	ret, err := pkg.SocketSend(conn, data, 4096)
 	if err != nil {
 		return
 	}
