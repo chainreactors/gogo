56bb7379f67fb48622587cd439e93dc5c5a9ceff
M09Ic <m09ician@gmail.com>
2022-08-31 00:56:32 +0800

    gorelease instead of bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-set name=gogo
-rm ./bin/*
-for /F %%i in ('git describe --abbrev^=0 --tags') do ( set gt_ver=%%i)
-set gt_key=%1
-
-go get sigs.k8s.io/yaml
-go generate gogo.go
-
-gox.exe -osarch="linux/amd64 linux/arm64 linux/arm linux/386 windows/amd64 linux/mips64 windows/386 darwin/amd64" -ldflags="-s -w -X 'github.com/chainreactors/gogo/v1/cmd.ver=%gt_ver%' -X 'github.com/chainreactors/gogo/v1/cmd.k=%gt_key%'" -gcflags="-trimpath=$GOPATH" -asmflags="-trimpath=$GOPATH" -output=".\bin\%name%_{{.OS}}_{{.Arch}}" .
