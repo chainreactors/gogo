7ff8d0909d0a2434db679138eb0d6df1746c0442
M09Ic <m09ician@gmail.com>
2022-10-24 15:54:07 +0800

    优化log输出

--- v2/internal/core/generator.go
+++ v2/internal/core/generator.go
@@ -150,7 +150,11 @@ func (gen *targetGenerator) genFromSpray(cidrs ipcs.CIDRs, portlist []string) {
 			if len(tmpPorts) >= 100 {
 				tmpPorts = tmpPorts[:100]
 			}
-			Log.Importantf("Processed Port: %s, found %d ports", strings.Join(tmpPorts, ",")+"......, "+port, Opt.AliveSum-tmpalive)
+			if len(tmpPorts) > 10 {
+				Log.Importantf("Processed Port: %s, found %d ports", strings.Join(tmpPorts, ",")+"......, "+port, Opt.AliveSum-tmpalive)
+			} else {
+				Log.Importantf("Processed Port: %s, found %d ports", strings.Join(tmpPorts, ","), Opt.AliveSum-tmpalive)
+			}
 			tmpPorts = []string{}
 		}
 	}
