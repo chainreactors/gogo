7ede3ac9291cb9dc1b4e13bcfc3ec4bf1008f05c
M09Ic <m09ician@gmail.com>
2023-05-31 16:43:39 +0800

    优化yaml2json.py脚本， 现在能添加任意数量的path file.

--- tools/yaml2json.py
+++ tools/yaml2json.py
@@ -1,5 +1,5 @@
 import click
-import yaml, json
+import yaml, json, os
 from functools import partial
 import warnings
 
@@ -10,19 +10,33 @@ def toFile(filename, content: str):
         f.write(content)
 
 
+def loadyaml(filename):
+    print("loading " + filename)
+    f = open(filename, "rb")
+    return yaml.load(f.read(), Loader=yaml.FullLoader)
+
 @click.command()
-@click.argument("files", nargs=-1, type=click.File("rb"))
+@click.argument("path", nargs=-1, type=click.Path(exists=True))
 @click.option('--outfile', '-f', help='output file')
-def main(files, outfile):
+def main(path, outfile):
     pocs = []
     if not outfile:
         outfile = "poc.json"
 
     print("save to " + outfile)
     outfunc = partial(toFile, outfile)
-    for file in files:
-        pocs.append(yaml.load(file.read(), Loader=yaml.FullLoader))
+    
+    for p in path:
+        if os.path.isdir(p):
+            for root, dirs, files in os.walk(p):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    if file_path.endswith("yml") or file_path.endswith("yaml"):
+                        pocs.append(loadyaml(file_path))
+        else:
+            pocs.append(loadyaml(p))
 
+    print("load %d pocs"%len(pocs))
     outfunc(json.dumps(pocs))
 
 if __name__ == '__main__':
