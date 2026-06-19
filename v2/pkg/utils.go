78f0b7ca8f6635e16a42d83b9fc88281ff41471a
Jah-yee <166608075+Jah-yee@users.noreply.github.com>
2026-06-19 14:12:40 +0800

    fix: correct typo unkown -> unknown in v2/pkg/utils.go
    Fixes comment describing port status return values.

--- v2/pkg/utils.go
+++ v2/pkg/utils.go
@@ -18,7 +18,7 @@ var (
 	DefaultMaxSize = 1024 * 16 // 16k
 )
 
-// return open: 0, closed: 1, filtered: 2, noroute: 3, denied: 4, down: 5, error_host: 6, unkown: -1
+// return open: 0, closed: 1, filtered: 2, noroute: 3, denied: 4, down: 5, error_host: 6, unknown: -1
 var PortStat = map[int]string{
 	0:  "open",
 	1:  "closed",
